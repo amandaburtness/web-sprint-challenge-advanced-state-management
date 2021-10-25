3ef35491852df0027a3fd14d4f6457498dbbb34e
Amanda Burtness <amandaburtness@gmail.com>
2021-10-24 22:00:48 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
@@ -8,12 +9,12 @@ import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import { fetchSmurfs } from "./actions";
 
 class App extends Component {
-  componentDidMount() {
-    axios.get('http://localhost:3333/smurfs')
-    .then(res => console.log(res))
-    .catch(err => console.log('Axios Error', err));
+  componentDidMount(){
+    console.log("didMount")
+    this.props.dispatch(fetchSmurfs())
   }
 
   render() {
@@ -30,7 +31,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null)(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
